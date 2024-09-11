6563fdead9f77bcc7fb9af30a4f9655d674f4fe3
Reshmafareed <163650782+Reshmafareed@users.noreply.github.com>
2024-09-11 15:00:24 +0500

    Add files via upload

---
+++ sandwiches.ts
@@ -0,0 +1,18 @@
+// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. 
+// The function should have one parameter that collects as many items as the function call 
+// provides, and it should print a summary of the sandwich that is being ordered. 
+// Call the function three times, using a different number of arguments each time.
+
+function makeSandwiche (item:string[]){
+    console.log('\nMaking your sandwich with:');
+
+    item.forEach(element => console.log("-" + element));
+
+    console.log('Enjoye your sandwich !\n');
+}
+
+makeSandwiche(['Ham','Cheese','Lettuce']);
+
+makeSandwiche(['Turkey','Bacon']);
+
+makeSandwiche(['Peanut butter','Jelly']);
